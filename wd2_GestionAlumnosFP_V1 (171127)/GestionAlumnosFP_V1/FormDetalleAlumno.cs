c06fae926b6a2bd93e5c9097afa62cfc4a999842
ALUM02 <ALUM02@PC02>
2017-11-29 10:21:49 +0100

    29/11/17

--- wd2_GestionAlumnosFP_V1 (171127)/GestionAlumnosFP_V1/FormDetalleAlumno.cs
+++ wd2_GestionAlumnosFP_V1 (171127)/GestionAlumnosFP_V1/FormDetalleAlumno.cs
@@ -56,7 +56,10 @@ namespace GestionAlumnosFP_V1
         private void btnAceptar_Click(object sender, EventArgs e)
         {
             if (HayErrorEnFormulario())
+            {
+                errorProvider1.Clear();
                 return;
+            }
 
 
             // actualizo el alumno
@@ -73,30 +76,66 @@ namespace GestionAlumnosFP_V1
 
         private bool HayErrorEnFormulario()
         {
-            errorProvider1.Clear();
+            string text = "";
             bool error = false;
+            string[] tabla = txbApellNom.Text.Split(',');
 
 
-            if (txbApellNom.Text == String.Empty || (txbTelefono.Text == String.Empty && txbMovil.Text == String.Empty) || cbGruposDetalle.Text == String.Empty)
+            if ((txbTelefono.Text == String.Empty && txbMovil.Text == String.Empty) || cbGruposDetalle.Text == String.Empty)
             {
+                text += "Debe haber un numero de contacto\n";
                 error = true;
             }
             if ((txbMovil.Text.Length != 9 || (txbMovil.Text.Substring(0, 1) != "6" && txbMovil.Text.Substring(0, 1) != "7")) && txbMovil.Text.Length != 0)
             {
+                text += "El movil debe tener 9 digitos y empezar por 6 o 7\n";
                 error = true;
                 errorProvider1.SetError(txbMovil, "Error de formato");
             }
             if ((txbTelefono.Text.Length != 9 || (txbTelefono.Text.Substring(0, 3) != "954" && txbTelefono.Text.Substring(0, 3) != "955")) && txbTelefono.Text.Length != 0)
             {
+                text += "El telefono debe tener 9 digitos y empezar por 954 o 955\n";
                 error = true;
                 errorProvider1.SetError(txbTelefono, "Error de formato");
             }
             if (!Regex.IsMatch(txbMail.Text, @"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*"))
             {
+                text += "Formato de correo electronico incorrecto";
                 error = true;
                 errorProvider1.SetError(txbMail, "Error de formato");
             }
-
+            if (txbDni.Text.Length == 10)
+            {
+                if (txbDni.Text[8] != '-' || (txbDni.Text[9] > 'Z' || txbDni.Text[9] < 'A'))
+                {
+                    text += "Formato de DNI incorrecto";
+                    error = true;
+                    errorProvider1.SetError(txbDni, "Error de formato");
+                }
+            }
+            else
+            {
+                text += "Formato de DNI incorrecto";
+                error = true;
+                errorProvider1.SetError(txbDni, "Error de formato");
+            }
+            if (tabla.Length == 2)
+            {
+                if (tabla[1][0] != ' ')
+                {
+                    text += "Formato de appelidos y nombre incorrecto";
+                    error = true;
+                    errorProvider1.SetError(txbApellNom, "Error de formato");
+                }
+            }
+            else
+            {
+                text += "Formato de appelidos y nombre incorrecto";
+                error = true;
+                errorProvider1.SetError(txbApellNom, "Error de formato");
+            }
+            if (error)
+                MessageBox.Show(text, "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
             return error;
         }
